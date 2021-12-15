2b3b27accf3970ebeab7719315825a5b8b2e9033
Marvin <marvin.warden@outlook.com>
2021-12-15 07:47:30 +0200

    working on tags for user input

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,44 +1,127 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
 export default function Searchbar({ searchCallback, clearCallback, updateFieldCallback, project, character, episode, line }) {
+
+    const [projectTags, setProjectTags] = useState([]);
+    const [characterTags, setCharacterTags] = useState([]);
+    const [episodeTags, setEpisodeTags] = useState([]);
+    const [lineTags, setLineTags] = useState([]);
+
     // TODO: implement input tokenization for user input during search query
     const re_space = new RegExp('^ *$');
     const valid_search = (re_space.test(project))
-                            && (re_space.test(character))
-                            && (re_space.test(episode))
-                            && (re_space.test(line));
+        && (re_space.test(character))
+        && (re_space.test(episode))
+        && (re_space.test(line));
+
+    //Add tags for user query input
+    const addTag = (e, tags, setTags) => {
+        if (e.key === "Enter" || e.keyCode === 13) {
+            if (e.target.value !== "") {
+                setTags([...tags, e.target.value]);
+                e.target.value = "";
+
+            }
+            
+        }
+        
+    };
+    //Remove query tags
+    const removeTag = (tagToRemove, removeTags, setRemoveTags) => {
+        setRemoveTags(
+            removeTags.filter((_, index) => {
+                return index !== tagToRemove;
+            })
+        );
+    };
+
 
     return (
         <div className='search-bar'>
             <form className='search-form'>
-                    <div className='input-fields'>
+                <div className='input-fields'>
+                    <div className='tag-container'>
+                        {projectTags.map((tag, index) => (
+                            <p className="tags" key={index}>
+                                {tag}
+                                <span>
+                                    <i onClick={() => removeTag(index, projectTags, setProjectTags)}>x</i>
+                                </span>
+                            </p>
+                        ))}
+
                         <input
+
                             placeholder='Projects'
                             onChange={(e) => { e.preventDefault(); updateFieldCallback('projects', e.target.value); }}
+                            onKeyDown={(e) => addTag(e, projectTags, setProjectTags)}
                             className='search-input'
-                            value={project}
+                            
                         />
+                    </div>
+                    <div className='tag-container'>
+                        {characterTags.map((tag, index) => (
+                            <p className="tags" key={index}>
+                                {tag}
+                                <span>
+                                    <i onClick={() => removeTag(index, characterTags, setCharacterTags)}>x</i>
+                                </span>
+                            </p>
+                        ))}
+
                         <input
+
                             placeholder='Characters'
                             onChange={(e) => { e.preventDefault(); updateFieldCallback('characters', e.target.value); }}
+                            onKeyDown={(e) => addTag(e, characterTags, setCharacterTags)}
                             className='search-input'
-                            value={character}
+                            
+
                         />
+                    </div>
+
+                    <div className='tag-container'>
+                        {episodeTags.map((tag, index) => (
+                            <p className="tags" key={index}>
+                                {tag}
+                                <span>
+                                    <i onClick={() => removeTag(index)}>x</i>
+                                </span>
+                            </p>
+                        ))}
+
                         <input
                             placeholder='Episodes'
                             onChange={(e) => { e.preventDefault(); updateFieldCallback('episodes', e.target.value); }}
+                            onKeyDown={(e) => addTag(e, setEpisodeTags)}
                             className='search-input'
-                            value={episode}
+                            
                         />
+                    </div>
+
+                    <div className='tag-container'>
+                        {lineTags.map((tag, index) => (
+                            <p className="tags" key={index}>
+                                {tag}
+                                <span>
+                                    <i onClick={() => removeTag(index)}>x</i>
+                                </span>
+                            </p>
+                        ))}
+
                         <input
                             placeholder='Lines'
                             onChange={(e) => { e.preventDefault(); updateFieldCallback('lines', e.target.value); }}
+                            onKeyDown={(e) => addTag(e, setLineTags)}
                             className='search-input'
-                            value={line}
+                            
                         />
                     </div>
-                    {/* <div className='form-btn'>
+
+
+                </div>
+                {/* <div className='form-btn'>
                         <button onClick={(e) => { e.preventDefault(); if (!valid_search) searchCallback(true); }} className='search-btn'>Search</button>
                         <button onClick={(e) => { e.preventDefault(); clearCallback(); }} className='search-btn'>Reset</button>
                     </div> */}
